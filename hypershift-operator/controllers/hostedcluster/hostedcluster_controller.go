71bc9db7cb9bcfc2124c27c39f7441afef711cdc
relyt0925 <lisowski@us.ibm.com>
2021-05-04 21:49:01 -0500

    up

--- hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
+++ hypershift-operator/controllers/hostedcluster/hostedcluster_controller.go
@@ -472,6 +472,8 @@ func reconcileHostedControlPlane(hcp *hyperv1.HostedControlPlane, hcluster *hype
 		hcp.Spec.Platform.AWS.NodePoolManagementCreds = corev1.LocalObjectReference{
 			Name: manifests.AWSNodePoolManagementCreds(hcp.Namespace).Name,
 		}
+	case hyperv1.IBMCloudPlatform:
+		hcp.Spec.Platform.Type = hyperv1.IBMCloudPlatform
 	case hyperv1.NonePlatform:
 		hcp.Spec.Platform.Type = hyperv1.NonePlatform
 	}

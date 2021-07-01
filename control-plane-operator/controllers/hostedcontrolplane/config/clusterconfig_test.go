f16fcdadce43254d48f4ccfd6402a9774f8af8c4
Cesar Wong <cewong@redhat.com>
2021-07-01 13:30:54 -0400

    Implement global configuration via embedded RawExtensions
    Adds a field to both the HostedCluster and HostedControlPlane to hold
    configuration. Wires in the configuration to the parameters used to
    render kube apiserver, controller manager, scheduler.

---
+++ control-plane-operator/controllers/hostedcontrolplane/config/clusterconfig_test.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"context"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	configv1 "github.com/openshift/api/config/v1"
+)
+
+var featureGateBytes = `
+apiVersion: config.openshift.io/v1
+kind: FeatureGate
+metadata:
+  name: cluster
+spec:
+  featureSet: LatencySensitive
+`
+
+func TestParseGlobalConfig(t *testing.T) {
+	items := []runtime.RawExtension{
+		{
+			Raw: []byte(featureGateBytes),
+		},
+	}
+
+	globalConfig, err := ParseGlobalConfig(context.Background(), items)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if globalConfig.FeatureGate == nil {
+		t.Fatalf("feature gate config not found")
+	}
+	if globalConfig.FeatureGate.Spec.FeatureSet != configv1.LatencySensitive {
+		t.Errorf("unexpected featureset: %q", globalConfig.FeatureGate.Spec.FeatureSet)
+	}
+}
